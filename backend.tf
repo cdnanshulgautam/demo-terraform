b041ca111c1e0306fb8416aa1c15fa1938042ed5
cdnanshulgautam <anshulgautam@cdnsol.com>
2024-11-08 18:55:37 +0530

    region change

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "github-actions-demo-22"
     key    = "github-actions-demo.tfstate"
-    region = "ap-northeast-2"
+    region = "ap-northeast-2a"
   }
 }

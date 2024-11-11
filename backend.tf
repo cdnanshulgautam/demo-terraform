0b075083dd5d1794e248b5484556e0f8ea69aafa
cdnanshulgautam <anshulgautam@cdnsol.com>
2024-11-11 12:46:38 +0530

    Bucket fix

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "github-actions-demo-22"
+    bucket = "github-actions-demo-33"
     key    = "github-actions-demo.tfstate"
     region = "ap-northeast-1"
   }
